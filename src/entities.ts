80011e6465e5cafea76aa9d104ebfaa9b4596a87
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-09-28 09:53:42 -0700

    try it out on another rhythm

--- src/entities.ts
+++ src/entities.ts
@@ -1,7 +1,7 @@
 import { EntityConfig } from '../../../src/compile/types'
 import { OscillatorName, VoiceType } from '../../../src/types'
 import { Scalar } from '../../../src/utilities/nominalTypes'
-import { fiveRhythm, fiveRhythmHafuhafu } from './rhythms'
+import { fiveRhythm, fiveRhythmHafuhafu, sevenRhythm, sevenRhythmHafu, sevenRhythmHafuhafu } from './rhythms'
 import { hafuhafu } from './hafuhafu'
 import sequence from '../../../src/utilities/sequence'
 
@@ -9,7 +9,12 @@ import sequence from '../../../src/utilities/sequence'
 const TO_AVOID_BLOW_OUT: Scalar = 0.2 as any
 
 const hafuhafuEntity: EntityConfig = {
-    notes: sequence([hafuhafu(fiveRhythm, 32), hafuhafu(fiveRhythmHafuhafu, 32)]),
+    // notes: sequence([hafuhafu(fiveRhythm, 32), hafuhafu(fiveRhythmHafuhafu, 32)]),
+    notes: sequence([
+        hafuhafu(sevenRhythm, 32),
+        hafuhafu(sevenRhythmHafu, 32),
+        hafuhafu(sevenRhythmHafuhafu, 32),
+    ]),
     voiceConfig: {timbre: OscillatorName.SQUARE, voiceType: VoiceType.OSCILLATOR},
     voiceGain: TO_AVOID_BLOW_OUT,
 }
--- src/rhythms.ts
+++ src/rhythms.ts
@@ -5,7 +5,16 @@ const fiveRhythm: Rhythm = to.Rhythm([0, 1, 0, 0, 1])
 
 const fiveRhythmHafuhafu: Rhythm = to.Rhythm([0, 0, 1, 1, 0])
 
+const sevenRhythm: Rhythm = to.Rhythm([0, 0, 0, 1, 0, 0, 1])
+
+const sevenRhythmHafu: Rhythm = to.Rhythm([0, 0, 0, 1, 0, 1, 0])
+
+const sevenRhythmHafuhafu: Rhythm = to.Rhythm([0, 0, 0, 0, 0, 1, 1])
+
 export {
     fiveRhythm,
     fiveRhythmHafuhafu,
+    sevenRhythm,
+    sevenRhythmHafu,
+    sevenRhythmHafuhafu,
 }
