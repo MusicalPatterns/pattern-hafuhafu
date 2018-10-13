b4d531d30cd07ffaebbd35339711c867cc8aa533
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-12 22:32:38 -0700

    separate song metadata from song config and song itself

--- src/compile.ts
+++ src/compile.ts
@@ -1,30 +1,36 @@
 import { buildEntity } from '../../../src/compile/buildEntity'
 import { EntityConfig, EntityConfigs } from '../../../src/compile/types'
-import { Song } from '../../../src/songTypes'
-import { Entities, Entity } from '../../../src/types'
+import { Song, SongConfig, SongID } from '../../../src/songTypes'
+import { Entity } from '../../../src/types'
 import { buildHafuhafuEntityConfigs, buildHafuhafuWithPitchCircularityEntityConfigs } from './entities'
 import { HafuhafuConfig, Rhythm } from './types'
 
-const hafuhafuCompile: (song: Song) => Promise<Entities> =
-    async (song: Song): Promise<Entities> => {
-        const config: HafuhafuConfig = song.config as HafuhafuConfig
+const hafuhafuCompile: (songConfig: SongConfig) => Promise<Song> =
+    async (songConfig: SongConfig): Promise<Song> => {
+        const config: HafuhafuConfig = songConfig.config as HafuhafuConfig
         const rhythm: Rhythm = config.rhythm
 
         const entityConfigs: EntityConfigs = buildHafuhafuEntityConfigs(rhythm)
 
-        return entityConfigs.map((entityConfig: EntityConfig): Entity =>
-            buildEntity(entityConfig, song))
+        return {
+            entities: entityConfigs.map((entityConfig: EntityConfig): Entity =>
+                buildEntity(entityConfig, songConfig)),
+            songId: SongID.HAFUHAFU,
+        }
     }
 
-const hafuhafuWithPitchCircularityCompile: (song: Song) => Promise<Entities> =
-    async (song: Song): Promise<Entities> => {
-        const config: HafuhafuConfig = song.config as HafuhafuConfig
+const hafuhafuWithPitchCircularityCompile: (songConfig: SongConfig) => Promise<Song> =
+    async (songConfig: SongConfig): Promise<Song> => {
+        const config: HafuhafuConfig = songConfig.config as HafuhafuConfig
         const rhythm: Rhythm = config.rhythm
 
         const entityConfigs: EntityConfigs = buildHafuhafuWithPitchCircularityEntityConfigs(rhythm)
 
-        return entityConfigs.map((entityConfig: EntityConfig): Entity =>
-            buildEntity(entityConfig, song))
+        return {
+            entities: entityConfigs.map((entityConfig: EntityConfig): Entity =>
+                buildEntity(entityConfig, songConfig)),
+            songId: SongID.HAFUHAFU_WITH_PITCH_CIRCULARITY,
+        }
     }
 
 export {
--- src/songs.ts
+++ src/songs.ts
@@ -1,4 +1,4 @@
-import { Song, SongID } from '../../../src/songTypes'
+import { SongConfig, SongID } from '../../../src/songTypes'
 import { Frequency, Scalar } from '../../../src/utilities/nominalTypes'
 import scale from '../../../src/utilities/scale'
 import { hafuhafuCompile, hafuhafuWithPitchCircularityCompile } from './compile'
@@ -11,39 +11,35 @@ const HAFUHAFU_BASE_FREQUENCY: Frequency = 50 as any
 // tslint:disable-next-line:no-any no-magic-numbers
 const HAFUHAFU_DURATION_SCALAR: Scalar = 25 as any
 
-const hafuhafu: Song = {
+const hafuhafuSongConfig: SongConfig = {
     compile: hafuhafuCompile,
     config: {
         baseFrequency: HAFUHAFU_BASE_FREQUENCY,
         durationScalar: HAFUHAFU_DURATION_SCALAR,
         rhythm: to.Rhythm([ 0, 1, 0, 0, 1 ]),
     },
-    description: 'rhythmic circularity; rhythms within themselves',
     entityConfigs: [],
-    formattedName: 'Hafuhafu',
-    id: SongID.HAFUHAFU,
     scales: [
         octaveSeries,
     ],
+    songId: SongID.HAFUHAFU,
 }
 
-const hafuhafuWithPitchCircularity: Song = {
+const hafuhafuWithPitchCircularitySongConfig: SongConfig = {
     compile: hafuhafuWithPitchCircularityCompile,
     config: {
         baseFrequency: HAFUHAFU_BASE_FREQUENCY,
         durationScalar: scale(HAFUHAFU_DURATION_SCALAR, HAFUHAFU_WITH_PITCH_CIRCULARITY_SCALAR),
         rhythm: to.Rhythm([ 0, 1, 0, 0, 1 ]),
     },
-    description: 'rhythmic circularity with extraneous and slipshod pitch circularity',
     entityConfigs: [],
-    formattedName: 'Hafuhafu (with pitch circularity)',
-    id: SongID.HAFUHAFU_WITH_PITCH_CIRCULARITY,
     scales: [
         octaveSeries,
     ],
+    songId: SongID.HAFUHAFU_WITH_PITCH_CIRCULARITY,
 }
 
 export {
-    hafuhafu,
-    hafuhafuWithPitchCircularity,
+    hafuhafuSongConfig,
+    hafuhafuWithPitchCircularitySongConfig,
 }
