5115d2ae0b362370a144372645edde2d608a198a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-30 22:12:54 -0800

    now all patterns export not just their snapshot but also their configuration so they can be customized if you also import the compiler

---
+++ package/index.d.ts
@@ -0,0 +1,8 @@
+// tslint:disable:no-implicit-dependencies
+
+import { Pattern } from '@musical-patterns/pattern';
+import { ThreadSpec } from '@musical-patterns/performer';
+declare const snapshot: ThreadSpec[];
+declare const hafuhafuPattern: Pattern;
+export { hafuhafuPattern };
+export default snapshot;
---
+++ package/index.js
@@ -0,0 +1,4 @@
+export { pattern as hafuhafuPattern } from './dist'
+
+import snapshot from './snapshot'
+export default snapshot
