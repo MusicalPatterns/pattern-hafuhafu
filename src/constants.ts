ea2b909eb05f99763cd263c45bf02ac417bc9f36
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-29 14:30:42 -0800

    1.0.81: ms

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
 // tslint:disable:no-magic-numbers
 
-import { Base, Block, Count, Frequency, Millisecond, Offset, Scalar, to } from '@musical-patterns/utilities'
+import { Base, Block, Count, Frequency, Milliseconds, Offset, Scalar, to } from '@musical-patterns/utilities'
 
 const HAFUHAFU_WITH_PITCH_CIRCULARITY_SCALAR: Scalar = to.Scalar(2)
 const BASE_FOR_GAIN_FADE: Base = to.Base(2)
 const PITCH_INDEX_BASE_OFFSET: Offset = to.Offset(1)
 const HAFUHAFU_INITIAL_BASE_FREQUENCY: Frequency = to.Frequency(50)
-const HAFUHAFU_INITIAL_BASE_DURATION: Millisecond = to.Millisecond(175)
+const HAFUHAFU_INITIAL_BASE_DURATION: Milliseconds = to.Milliseconds(175)
 const HAFUHAFU_INITIAL_BLOCK: Block = to.Block([ 0, 1, 0, 0, 1 ])
 const HAFUHAFU_INITIAL_ITERATION_LENGTH: Count = to.Count(16)
 
