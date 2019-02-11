424bff44e6107cb4e9b1ea2d0af792ea451f708a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-10 17:01:54 -0800

    1.0.99: nominal types overhaul

--- src/material/scalars.ts
+++ src/material/scalars.ts
@@ -1,9 +1,9 @@
-import { apply, from, Scalar, to, zeroAndPositiveIntegers } from '@musical-patterns/utilities'
+import { apply, from, Integer, Scalar, to, zeroAndPositiveIntegers } from '@musical-patterns/utilities'
 import { HafuhafuSpec } from '../types'
 
 const buildScalars: (spec: HafuhafuSpec) => Scalar[] =
     ({ pitchStep }: HafuhafuSpec): Scalar[] =>
-        zeroAndPositiveIntegers.map((integer: number): Scalar =>
+        zeroAndPositiveIntegers.map((integer: Integer): Scalar =>
             to.Scalar(from.Base(apply.Power(pitchStep, to.Power(integer)))))
 
 export {
