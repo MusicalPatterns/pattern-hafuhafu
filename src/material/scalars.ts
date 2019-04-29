b29ffd03bb0ac649a797967b79889feb4444f842
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-29 11:42:54 -0700

    1.0.193: typed pow and log

--- src/material/scalars.ts
+++ src/material/scalars.ts
@@ -1,4 +1,4 @@
-import { as, Frequency, Integer, Logarithm, Pitch, range, Scalar, use } from '@musical-patterns/utilities'
+import { as, Frequency, Integer, Pitch, pow, range, Scalar } from '@musical-patterns/utilities'
 import { HafuhafuSpecs } from '../spec'
 import { ENOUGH_PITCH_SCALARS_THAT_NO_ONE_WILL_PROBABLY_EVER_NOTICE } from './constants'
 
@@ -6,9 +6,9 @@ const computePitchScalars: (specs: HafuhafuSpecs) => Array<Scalar<Pitch>> =
     ({ pitchStep }: HafuhafuSpecs): Array<Scalar<Pitch>> =>
         range(ENOUGH_PITCH_SCALARS_THAT_NO_ONE_WILL_PROBABLY_EVER_NOTICE)
             .map((integer: Integer) =>
-                as.Scalar<Pitch>(as.number(use.Power(
+                as.Scalar<Pitch>(as.number(pow(
                     pitchStep,
-                    as.Power<Logarithm<Frequency>>(integer),
+                    as.Power<Frequency>(integer),
                 ))),
             )
 
