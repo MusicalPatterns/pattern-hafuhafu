76ba505dadc2a34c56a2d002a377b6b913186531
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-05-03 10:59:45 -0700

    1.0.201: dont use our as

--- src/nominals.ts
+++ src/nominals.ts
@@ -1,14 +1,5 @@
-import { computeNominalInterface, DUMMY_VALUE_FOR_COMPUTING_NOMINAL_INTERFACE } from '@musical-patterns/utilities'
-
 type Layer = Number & { _NominalBrand: 'Layer' }
 
-const { as } = computeNominalInterface({
-    number: {
-        Layer: DUMMY_VALUE_FOR_COMPUTING_NOMINAL_INTERFACE as Layer,
-    },
-})
-
 export {
-    as,
     Layer,
 }
